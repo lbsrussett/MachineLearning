24237da260b384ed22e769ecc002306a7fafdef2
lbsrussett <lbsrussett@gmail.com>
2017-12-05 14:54:31 -0700

    current ACO & corrected text files without class labels

--- MachineLearning/src/proj4/Ant.java
+++ MachineLearning/src/proj4/Ant.java
@@ -1,24 +1,68 @@
 package proj4;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Ant {
 	
-	private int antID;
+	/*private int antID;
 	private boolean isCarrying = false;
-	private Point point;
-	private ArrayList<Cluster> solution = new ArrayList<Cluster>();
+	private Point point;*/
+	private int[] solution;
+	private double[][] pheromones;
+	private int clustNum;
 	
-	public Ant(int antID) {
-		this.antID = antID;
+	public Ant(int clustNum, double[][] pheromones) {
+		solution = new int[pheromones.length];
+		initSolutions();
 	}
-	public void antSearch(Point[][] grid) {
+	private void initSolutions() {
+		for(int i = 0; i < solution.length; i++) {
+			Random rand = new Random();
+			solution[i] = rand.nextInt(2) + 1;
+		}
 		
 	}
-	public ArrayList<Cluster> returnSolution() {
+	public void antSearch(double[][] pheromones) {
+		solution = new int[pheromones.length];
+		this.pheromones = pheromones;
+		this.clustNum = clustNum;
+		createSolution();
+	}
+	private void createSolution() {
+		for(int i = 0; i < solution.length; i++) {
+			double highest = 0;
+			for(int j = 0; j < clustNum; j++) {
+				if(pheromones[i][j] > highest) {
+					highest = pheromones[i][j];
+					solution[i] = j+1;
+				}
+			}
+		}
+		
+			/*if(clustNum == 2) {
+				if(prob[i] > 0.50) {
+					solution[i] = 1;
+				}
+				else
+					solution[i] = 2;
+			}
+			else if(clustNum == 3) {
+				if(prob[i] < 0.33) {
+					solution[i] = 1;
+				}
+				else if (0.33 < prob[i] && prob[i] < 0.66) {
+					solution[i] = 2;
+				}
+				else
+					solution[i] = 3;
+			}*/
+		
+	}
+	public int[] returnSolution() {
 		return solution;
 	}
-	public boolean carrying() {
+	/*public boolean carrying() {
 		return isCarrying;
 	}
 	public void pickUp(Point p) {
@@ -42,4 +86,11 @@ public class Ant {
 		distance = Math.sqrt(distance);
 		return distance;
 	}
+	private boolean checkCluster(Point p) {
+		if(p.unclassified()) {
+			return false;
+		}
+		else
+			return true;
+	}*/
 }
--- MachineLearning/src/proj4/Cluster.java
+++ MachineLearning/src/proj4/Cluster.java
@@ -30,11 +30,48 @@ public class Cluster {
 
 	public void removeCluster() {
 			for(Point p : points) {
-				//removePoint(p);
 				p.updateUnclassified(true);
 			}
 	}
 	public int clusterSize() {
 		return points.size();
+	}
+	public double fitness() {
+		Point center = getCenter();
+		double fitness = 0;
+		for(Point p : points) {
+			if(!p.equals(center)) {
+				fitness += euclideanDistance(center.getValues(), p.getValues());
+			}
+		}
+		return fitness;
+	}
+ 	private Point getCenter() {
+		Point center = null;
+		for(int i = 0; i < points.size(); i++) {
+			Point temp = points.get(i);
+			double tempAvg = Double.MAX_VALUE;
+			double average = 0;
+			for(int j = 0; j < points.size(); j++) {
+				if(!temp.equals(points.get(j))) {
+					average += euclideanDistance(temp.getValues(), points.get(j).getValues());
+				}
+			}
+			if(average < tempAvg) {
+				tempAvg = average;
+				center = temp;
+			}
+		}
+		return center;
+	}
+	private double euclideanDistance(double[] point1, double[] point2) {
+		double distance = 0;
+		for(int i = 0; i < point1.length; i++) {
+			double element = (point2[i] - point1[i]);
+			element = Math.pow(element, 2);
+			distance += element;
+		}
+		distance = Math.sqrt(distance);
+		return distance;
 	}
 }
